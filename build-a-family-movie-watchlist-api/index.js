b6e3d55f24c99f1159a0d9d18b0b3c3a8db11123
Reizi_ori <kanalnanaokhehe@gmail.com>
2026-09-07 04:40:11 +0000

    push

--- build-a-family-movie-watchlist-api/index.js
+++ build-a-family-movie-watchlist-api/index.js
@@ -1,20 +1,26 @@
 import express from "express";
 import helmet from "helmet";
 
-import watchlistRoutes from "./routes/watchlist";
+import watchlistRoutes from "./routes/watchlist.js";
 
 const PORT = process.env.PORT;
 const app = express();
 
+// Middleware keamanan HTTP headers
 app.use(helmet());
+
+// Middleware agar Express bisa membaca body JSON dari request
 app.use(express.json());
 
+// Route untuk cek server aktif
 app.get("/", (req, res) => {
   res.send("Family Movie Watchlist API");
 });
 
+// Semua route watchlist diakses dengan prefix /api/watchlist
 app.use("/api/watchlist", watchlistRoutes);
 
+// Jalankan server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
 });
